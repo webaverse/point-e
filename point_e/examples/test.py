29126a48ee25a56c57892e7048874c3c386ab9d7
Avaer Kazmer <a@modules.io>
2023-01-16 06:36:13 -0500

    Update test.py

--- point_e/examples/test.py
+++ point_e/examples/test.py
@@ -115,5 +115,6 @@ samples = None
 for x in tqdm(sampler.sample_batch_progressive(batch_size=1, model_kwargs=dict(images=[generated_image]))):
     samples = x
 
+samples = samples.cpu()
 samples = samples.numpy()
 print(samples)

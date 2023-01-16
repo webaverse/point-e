15c3dca7893e511df538ebbb303e5052ada18660
Avaer Kazmer <a@modules.io>
2023-01-16 08:52:53 -0500

    Update test.py

--- point_e/examples/test.py
+++ point_e/examples/test.py
@@ -27,6 +27,10 @@ from point_e.models.download import load_checkpoint
 from point_e.models.configs import MODEL_CONFIGS, model_from_config
 from point_e.util.plotting import plot_point_cloud
 
+import sys
+
+# read the first argument
+arg = sys.argv[1]
 
 # In[2]:
 
